f9f56f0647f2afc7a5e6664230e890beb42f4ac3
Antucan <antherpra@gmail.com>
2024-11-25 17:15:22 +0100

    commiting like alabau

--- no_puntuable/no_puntuable.js
+++ no_puntuable/no_puntuable.js
@@ -1,6 +1,18 @@
+//ex1
 let llista_numeros = new Array(5);
+let listaDiv = document.getElementById("array1")
+let resultats = document.getElementById("resultats");
+
 for (i = 0; i < llista_numeros.length; i++) {
-    llista_numeros[i] = Math.floor(Math.random()*10);;
-    
+    llista_numeros[i] = Math.floor(Math.random() * 10) + 1;
+    listaDiv.innerHTML += "<div id=nums1>" + llista_numeros[i] + "</div>";
+    //ex3
+    resultats.innerHTML = llista_numeros[0] + " i " + llista_numeros[4];
 }
-console.log(llista_numeros);
+//ex2
+let llista_bidimensional = new Array(10);
+let listaDiv2 = document.getElementById("array2");
+for (i = 0; i < llista_bidimensional.length; i++) {
+    llista_bidimensional[i] = Math.floor(Math.random() * 10) + 1;
+    listaDiv2.innerHTML += "<div id=nums2>" + llista_bidimensional[i] + "</div>";
+}
