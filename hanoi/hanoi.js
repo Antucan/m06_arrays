aa145e7e58b7ce44f4ad87b9d11bacaad00d4801
Antucan <antherpra@gmail.com>
2024-11-26 19:31:35 +0100

    updating

--- hanoi/hanoi.js
+++ hanoi/hanoi.js
@@ -5,13 +5,14 @@ let torre3 = document.getElementById("torre3");
 
 //elemntos de js
 document.getElementById("start").addEventListener("click", function () {
+    torre1.innerHTML = "";
     let numDiscs = document.getElementById("numDiscs");
     let pila1 = [];
     let num = 1;
 
     for (i = 0; i < numDiscs.value; i++) {
         pila1[i] = num;
+        torre1.innerHTML += "<div>" + pila1[i] + "</div><br>";
         num++;
     }
-    console.log(pila1);
 });
